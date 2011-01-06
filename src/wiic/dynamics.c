8dc9b7b36b7694e70ff044e85a100553f098a885
gabriele82rm <gabriele82rm@fe444ef7-8870-402c-b3f2-cc32b5d63832>
2011-01-06 21:35:55 +0000

    different versus for roll
    git-svn-id: https://wiic.svn.sourceforge.net/svnroot/wiic@43 fe444ef7-8870-402c-b3f2-cc32b5d63832

--- src/wiic/dynamics.c
+++ src/wiic/dynamics.c
@@ -95,7 +95,7 @@ void calculate_orientation(struct accel_t* ac, struct vec3b_t* accel, struct ori
 	/* if it is over 1g then it is probably accelerating and not reliable */
 	if (abs(accel->x - ac->cal_zero.x) <= ac->cal_g.x) {
 		/* roll */
-		x = RAD_TO_DEGREE(atan2f(x, z));
+		x = -RAD_TO_DEGREE(atan2f(x, z));
 
 		orient->roll = x;
 		orient->a_roll = x;
@@ -103,7 +103,7 @@ void calculate_orientation(struct accel_t* ac, struct vec3b_t* accel, struct ori
 
 	if (abs(accel->y - ac->cal_zero.y) <= ac->cal_g.y) {
 		/* pitch */
-		y = RAD_TO_DEGREE(atan2f(y, z));
+		y = RAD_TO_DEGREE(asin(y));
 
 		orient->pitch = y;
 		orient->a_pitch = y;
