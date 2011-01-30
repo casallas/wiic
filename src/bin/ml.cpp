5fc79ea45731d4499d2cbe880e071f667a608778
gabriele82rm <gabriele82rm@fe444ef7-8870-402c-b3f2-cc32b5d63832>
2011-01-30 17:34:56 +0000

    fix
    git-svn-id: https://wiic.svn.sourceforge.net/svnroot/wiic@53 fe444ef7-8870-402c-b3f2-cc32b5d63832

--- src/bin/ml.cpp
+++ src/bin/ml.cpp
@@ -4,7 +4,9 @@ int main(int argc, char **argv) {
 
   
     if (argc<2) {
-		cout << "Use  " << argv[0] << " <KNN|Bayes|SVN|DT|ANN|Boost|RT> <training_data_ratio> <data_file1> ... <data_fileN>" << endl;
+		cout << "wiic-ml allows to recognize a set of gestures using OpenCV machine learning library." << endl;
+		cout << "wiic-ml is part of WiiC (http://wiic.sf.net)" << endl << endl;
+		cout << "Usage:  " << argv[0] << " <KNN|Bayes|SVN|DT|ANN|Boost|RT> <training_data_ratio> <data_file1> ... <data_fileN>" << endl;
 		cout << "e.g. " << argv[0] << " KNN 0.75 file1.log file2.log" << endl;
 		cout << "======= Supported Algorithms =======" << endl;
 		cout << "KNN: k-nearest neighbor" << endl;
